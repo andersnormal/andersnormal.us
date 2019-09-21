0674b7e2b3a9f9e074f5068ab40223663d76e69e
Sebastian Döll <sebastian@katallaxie.me>
2019-09-21 17:18:53 +0200

    chore: init repo

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Router, Link, Route } from 'svelte-routing'
+  import NavLink from './components/NavLink.svelte'
+  import Home from './routes/Home.svelte'
+  import About from './routes/About.svelte'
+
+  export let url = ''
+</script>
+
+<Router {url}>
+  <nav>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="about">About</NavLink>
+  </nav>
+  <div class="w-full max-w-screen-xl mx-auto px-6">
+    <Route path="about" component={About} />
+    <Route path="/" component={Home} />
+  </div>
+</Router>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let name = 'test2'
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
