3f46d99f6d4d020fdbf9d38ad97e5a2b63dcf162
Sebastian Döll <sebastian@katallaxie.me>
2021-01-28 14:01:10 +0100

    wip: adding post process

---
+++ .serverless_nextjs/assets/_next/static/chunks/1bfc9850.469da6a5aa3e5699dafb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[6],{ma3e:function(t,n,c){"use strict";c.d(n,"a",(function(){return e}));var a=c("Lnxd");function e(t){return Object(a.a)({tag:"svg",attr:{viewBox:"0 0 576 512"},child:[{tag:"path",attr:{d:"M336.2 64H47.8C21.4 64 0 85.4 0 111.8v288.4C0 426.6 21.4 448 47.8 448h288.4c26.4 0 47.8-21.4 47.8-47.8V111.8c0-26.4-21.4-47.8-47.8-47.8zm189.4 37.7L416 177.3v157.4l109.6 75.5c21.2 14.6 50.4-.3 50.4-25.8V127.5c0-25.4-29.1-40.4-50.4-25.8z"}}]})(t)}}}]);
+//# sourceMappingURL=1bfc9850.469da6a5aa3e5699dafb.js.map
