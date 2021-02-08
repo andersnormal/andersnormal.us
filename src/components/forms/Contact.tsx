db6ee43a65a8551f49efac2a4d516a3cdaf26235
Sebastian Döll <sebastian@katallaxie.me>
2021-02-08 12:08:09 +0100

    wip: form messaging and wizard

--- src/components/forms/Contact.tsx
+++ src/components/forms/Contact.tsx
@@ -1,41 +1,23 @@
 import React, { useState, useRef } from 'react'
 import { useForm } from 'react-hook-form'
 import {
-  Grid,
   Box,
-  Fade,
-  Text,
   EASINGS,
   Input,
   FormControl,
   FormLabel,
-  Textarea,
-  RadioGroup,
-  Stack,
-  Radio,
+  Select,
   InputRightAddon,
   Flex,
-  InputLeftAddon,
   SlideFade,
   IconButton,
-  Button,
-  EditableInput,
-  forwardRef,
-  Spacer,
-  FormErrorMessage,
   Progress,
   Heading,
-  FormHelperText,
   InputGroup
 } from '@chakra-ui/react'
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
-
-const schema = yup.object().shape({
-  email: yup.string().required(),
-  name: yup.string().required()
-})
 
 const transitions = {
   enter: {
@@ -77,66 +59,105 @@ const variants = {
   }
 }
 
-const Step = ({
-  name,
+const StepInput = ({
   inputRef,
-  placeholder,
-  label,
-  show,
   register,
-  validating,
+  name,
+  isSubmitting,
+  placeholder,
   onSubmit
 }) => {
   return (
-    <Box display={show ? 'block' : 'none'}>
+    <Input
+      focusBorderColor="none"
+      fontSize="5xl"
+      name={`${name}`}
+      ref={el => {
+        inputRef.current[name] = el
+        register(el)
+      }}
+      isDisabled={isSubmitting}
+      borderRadius="0"
+      border="none"
+      background="gray.100"
+      placeholder={placeholder}
+      py={12}
+      onKeyPress={e => {
+        if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+          onSubmit()
+        }
+      }}
+    />
+  )
+}
+
+const StepSelect = ({
+  children,
+  name,
+  inputRef,
+  placeholder,
+  register,
+  isSubmitting,
+  onSubmit
+}) => {
+  return (
+    <Select
+      focusBorderColor="none"
+      fontSize="5xl"
+      placeholder={placeholder}
+      isDisabled={isSubmitting}
+      height="6rem"
+      background="gray.100"
+      borderRadis="0"
+      lineHeight="inherit"
+      border="none"
+      name={`${name}`}
+      ref={el => {
+        inputRef.current[name] = el
+        register(el)
+      }}
+      onKeyPress={e => {
+        if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+          onSubmit()
+        }
+      }}
+    >
+      {children}
+    </Select>
+  )
+}
+
+const Step = ({ cmp: Component, ...props }) => {
+  return (
+    <Box display={props.show ? 'block' : 'none'}>
       <FormControl
-        id={name}
-        htmlFor={name}
+        id={props.name}
+        htmlFor={props.name}
         py={6}
         padding={0}
         colorScheme="black"
       >
-        <SlideFade in={show} variants={variants} offsetY={32}>
+        <SlideFade in={props.show} variants={variants} offsetY={32}>
           <FormLabel>
             <Heading as="h3" size="2xl" py={4}>
-              {label}
+              {props.label}
             </Heading>
           </FormLabel>
         </SlideFade>
         <InputGroup>
-          <Input
-            focusBorderColor="none"
-            fontSize="5xl"
-            name={`${name}`}
-            ref={el => {
-              inputRef.current[name] = el
-              register(el)
-            }}
-            borderRadius="0"
-            border="none"
-            background="gray.100"
-            placeholder={placeholder}
-            py={12}
-            onKeyPress={e => {
-              if (e.code === 'Enter' || e.code === 'NumpadEnter') {
-                onSubmit()
-              }
-            }}
-          />
+          <Component {...props} />
           <InputRightAddon border="none" borderRadius="0" height="auto">
             <IconButton
               aria-label="Next"
               color="gray.400"
-              onClick={() => onSubmit()}
+              onClick={() => props.onSubmit()}
               icon={<ArrowForwardIcon boxSize={12} />}
               bg="transparent"
-              isLoading={validating}
+              isDisabled={props.isSubmitting}
+              isLoading={props.validating}
             />
           </InputRightAddon>
         </InputGroup>
-        {/* <FormErrorMessage>
-            {errors.email && errors.email.message}
-          </FormErrorMessage> */}
       </FormControl>
     </Box>
   )
@@ -147,72 +168,99 @@ const steps = [
     key: 'full_name',
     label: 'What is your name?',
     placeholder: 'Sherlock Holmes',
-    validation: yup.string().required(),
-    Component: Step
+    validation: yup.string().label('Full Name').required(),
+    Component: StepInput
   },
   {
     key: 'business',
     label: 'What is your business name?',
     placeholder: 'Holmes & Co',
-    validation: yup.string().required(),
-    Component: Step
+    validation: yup.string().label('Business name').required(),
+    Component: props => (
+      <StepSelect {...props}>
+        <option value="">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </StepSelect>
+    )
   },
   {
     key: 'email',
     label: 'What is your email address?',
     placeholder: 'sherlock@holmes.co.uk',
     validation: yup.string().email().required(),
-    Component: Step
+    Component: StepInput
   },
   {
     key: 'conferencing',
     label: 'What is your preference?',
     placeholder: 'sherlock@holmes.co.uk',
-    Component: Step
+    Component: StepInput
   },
   {
     key: 'solution',
     label: 'What do you want to talk about?',
     placeholder: 'sherlock@holmes.co.uk',
-    Component: Step
+    Component: StepInput
   }
 ]
 
 export const Contact = () => {
   const [step, setStep] = useState(0)
   const inputRef = useRef([])
-  const { handleSubmit, errors, register, trigger, formState } = useForm({
-    resolver: yupResolver(
-      yup
-        .object()
-        .shape(
-          steps.reduce(
-            (prev, curr) => ({ ...prev, [curr.key]: curr.validation }),
-            {}
+  const formRef = useRef()
+  const { handleSubmit, errors, register, reset, trigger, formState } = useForm(
+    {
+      resolver: yupResolver(
+        yup
+          .object()
+          .shape(
+            steps.reduce(
+              (prev, curr) => ({ ...prev, [curr.key]: curr.validation }),
+              {}
+            )
           )
-        )
-    )
-  })
+      )
+    }
+  )
 
-  const onSubmit = data => {
-    console.log(data)
-    return
+  const onSubmit = async data => {
+    function timeout(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
+    await timeout(3000)
+
+    reset()
+    setStep(() => 0)
+    const next = steps[0].key
+    inputRef.current[next].focus()
+
+    return true
   }
 
   const onClick = async key => {
-    if (await trigger(key)) {
-      setStep(() => step + 1)
-      const next = steps[steps.findIndex(step => step.key === key) + 1].key
-      inputRef.current[next].focus()
+    if (!(await trigger(key))) {
+      return
     }
+
+    if (step + 1 === steps.length) {
+      await handleSubmit(onSubmit)()
+      return
+    }
+
+    setStep(() => step + 1)
+    const next = steps[steps.findIndex(step => step.key === key) + 1].key
+    inputRef.current[next].focus()
   }
 
   return (
     <Box width="100%">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form ref={formRef} onSubmit={handleSubmit(onSubmit)}>
         <Box>
           {steps.map(({ Component, key, ...props }) => (
-            <Component
+            <Step
+              cmp={Component}
               key={`${key}`}
               name={key}
               show={steps[step].key === key}
@@ -220,6 +268,7 @@ export const Contact = () => {
               onSubmit={() => onClick(key)}
               validating={formState.isValidating}
               inputRef={inputRef}
+              isSubmitting={formState.isSubmitting}
               {...props}
             />
           ))}
@@ -228,45 +277,18 @@ export const Contact = () => {
               colorScheme="gray"
               background="gray.200"
               value={(step / steps.length) * 100}
+              isIndeterminate={formState.isSubmitting}
             />
             <Flex justifyContent="flex-end">
+              <Heading as="h4" size="md" colorScheme="gray" flexGrow={1}>
+                {Object.values(errors)?.shift()?.message}
+              </Heading>
               <Heading as="h4" size="md" colorScheme="gray">
                 {`${step + 1} / ${steps.length}`}
               </Heading>
             </Flex>
           </Box>
         </Box>
-
-        {/* <Flex justifyContent="space-between" wrap="wrap">
-          <FormControl id="name" py={6}>
-            <Input type="name" placeholder="Start by typing your name..." />
-          </FormControl>
-          <FormControl id="business" width={{ base: '100%', lg: '48%' }} py={6}>
-            <FormLabel>Business name</FormLabel>
-            <Input type="business" placeholder="Holmes & CO" />
-          </FormControl>
-          <FormControl id="email" width={{ base: '100%', lg: '48%' }} py={6}>
-            <FormLabel>Email address</FormLabel>
-            <Input type="email" placeholder="sherlock@holmes.co.uk" />
-          </FormControl>
-          <FormControl id="message" width={{ base: '100%', lg: '48%' }} py={6}>
-            <FormLabel>Favorite conferencing tool</FormLabel>
-            <RadioGroup>
-              <Stack direction="row">
-                <Radio value="1">Google Meet</Radio>
-                <Radio value="2">Microsoft Teams</Radio>
-                <Radio value="3">Slack</Radio>
-              </Stack>
-            </RadioGroup>
-          </FormControl>
-          <FormControl id="message" py={6}>
-            <FormLabel>Message</FormLabel>
-            <Textarea placeholder="Here is a sample placeholder" />
-          </FormControl>
-          <Box>
-            <Button>Send</Button>
-          </Box>
-        </Flex> */}
       </form>
     </Box>
   )
